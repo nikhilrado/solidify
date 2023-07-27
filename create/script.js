fd7e5547535c1bc4de9d64e072c79a32eab9ac06
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2023-07-27 16:07:11 -0400

    refactor: updated style + refactord (CU-8678fyggt) - added much better comments - rearranged code - renamed variables

--- create/script.js
+++ create/script.js
@@ -1,4 +1,5 @@
-// returns the strong of the cookie with the name cname
+// returns the string of the cookie with the name cookieName
+// from w3schools https://www.w3schools.com/js/js_cookies.asp
 function getCookie(cookieName) {
   let name = cookieName + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -15,26 +16,6 @@ function getCookie(cookieName) {
   return "";
 }
 
-var jsondata;
-window.addEventListener("load", function () {
-  const password = document.getElementById("o-name");
-
-  password.addEventListener("focus", (event) => {
-    event.target.style.background = "#51b851";
-    console.log("fetch initiated");
-
-    var URL =
-      "https://api.solidify.ortanatech.com/action?action=upload-model&uuid=";
-    fetch(URL + getCookie("uuid"))
-      .then((response) => response.json())
-      .then(function (json) {
-        jsondata = json;
-        console.log(jsondata);
-        document.getElementById("payy").style.backgroundColor = "lightgreen";
-      });
-  });
-});
-
 // returns a Stripe checkout link
 function getCheckoutLink(shipping_countryf, obj_name) {
   var URL = "https://api.solidify.ortanatech.com/action/";
@@ -47,28 +28,31 @@ function getCheckoutLink(shipping_countryf, obj_name) {
     body: JSON.stringify({
       action: "checkout",
       uuid: getCookie("uuid"),
-      price: parseFloat(jsondata.materials[62].basePrice),
-      shipping_country: "US",
+      price: parseFloat(jsonPriceInfoData.materials[62].basePrice),
+      shipping_country: "US", // TODO: implement this
       prod_name: obj_name,
     }),
   }).then(function (response) {
     return response.text().then(function (text) {
       console.log(text);
-      window.location.href = text;
+      window.location.href = text; // redirect to Stripe checkout page
     });
   });
 }
 
+// called when pay button is clicked
 function pay() {
   console.log("pay");
-  //window.location.href =
+  //window.location.href = 
   getCheckoutLink(
     document.getElementById("shipping").value,
     document.getElementById("o-name").value
   );
 }
 
-function STLViewer(model, elementID) {
+// sets the 3d model viewbox then calls loadModel()
+// (inserts the stl file "model" into the html element with id "elementID")
+function prepareSTLViewer(model, elementID) {
   var elem = document.getElementById(elementID);
   camera = new THREE.PerspectiveCamera(
     70,
@@ -89,7 +73,7 @@ function STLViewer(model, elementID) {
     false
   );
 
-  //controls
+  // controls
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
   controls.rotateSpeed = 0.5;
@@ -98,6 +82,7 @@ function STLViewer(model, elementID) {
   controls.autoRotate = true;
   controls.autoRotateSpeed = 0.75;
 
+  // lighting
   scene = new THREE.Scene();
   scene.add(new THREE.HemisphereLight(0xffffff, 1.5));
   const light = new THREE.PointLight(0xffffff, 1, 100);
@@ -107,10 +92,11 @@ function STLViewer(model, elementID) {
   light2.position.set(-50, 50, 50);
   scene.add(light2);
 
-  loadit(model);
+  loadModel(model);
 }
 
-function loadit(model) {
+// loads the stl file "model" into the scene
+function loadModel(model) {
   new THREE.STLLoader().load(model, function (geometry) {
     var material = new THREE.MeshPhongMaterial({
       color: 0xff5533,
@@ -120,7 +106,7 @@ function loadit(model) {
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    console.log(mesh);
+    //console.log(mesh);
     //scene.remove(mesh);
 
     var middle = new THREE.Vector3();
@@ -147,8 +133,6 @@ function loadit(model) {
   });
 }
 
-var URL = "https://api.solidify.ortanatech.com/3d/csv?csv=";
-
 // a hash function from https://stackoverflow.com/a/52171480
 const cyrb53 = (str, seed = 0) => {
   let h1 = 0xdeadbeef ^ seed,
@@ -166,52 +150,69 @@ const cyrb53 = (str, seed = 0) => {
   return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
 
-function load_stl(csv) {
-  csv = csv.replace(/\s/g, ""); // remove whitespace
+function loadStl(csv) {
+  const generateModelAPIURL = "https://api.solidify.ortanatech.com/3d/csv?csv=";
 
+  // check if current data is same as last saved data,
+  // if so skip api call and display cached model
+  csv = csv.replace(/\s/g, ""); // remove whitespace
   csvHash = cyrb53(csv);
   if (csvHash == sessionStorage.getItem("csvHash")) {
-    console.log("same csv");
+    console.log("used cached model");
     stlLink = sessionStorage.getItem("stlLink");
+    prepareSTLViewer(stlLink, "model");
     document.getElementById("download-stl-link").href = stlLink;
-    STLViewer(stlLink, "model");
     return;
-  } else {
-    sessionStorage.setItem("csvHash", csvHash);
   }
 
-  fetch(URL + csv + "&uuid=" + getCookie("uuid"))
+  // fetch the stl link from the Solidify API
+  fetch(generateModelAPIURL + csv + "&uuid=" + getCookie("uuid"))
     .then(function (u) {
       return u.json();
     })
-    .then(function (json) {
-      console.log(json);
-      //loadit(json);
-      stlLink = json;
+    .then(function (stlLink) {
+      console.log("created new model from Solidify API");
       document.getElementById("download-stl-link").href = stlLink;
       sessionStorage.setItem("stlLink", stlLink);
-      STLViewer(json, "model"); // calling and passing json to another function processPriceInfo
+      sessionStorage.setItem("csvHash", csvHash);
+      prepareSTLViewer(stlLink, "model"); // calling and passing json to another function processPriceInfo
     });
 }
-//var data = prompt("enter some data")
-console.log(location.search);
-const params = new URLSearchParams(location.search);
-var param_data = params.get("data");
-console.log("fdfd:" + param_data);
 
-// will load stl with query param "data" value or session storage data
-if (param_data != null) {
-  load_stl(param_data);
-} else {
-  var data = getCookie("data");
-  data = sessionStorage.getItem("data");
-
-  console.log(data);
-  load_stl(data);
-}
-
-//load_stl([2,3,1,3,4,2,5,2,3,.5,2])
-
+// runs when the page is loaded (all external scripts are loaded)
+var jsonPriceInfoData; // global scope var
 window.onload = function () {
-  //STLViewer("https://3ddata.nikhilrado.repl.co/ex-scripts/will-ronan.stl", "model")
+  // when object name input is focused, fetch the price info
+  // the Solidify API will upload model to Shapeways and get an estimate
+  // this operation takes ~30 seconds so the other fields delay
+  // when the pay button turns green it can be clicked to Stripe checkout page
+  const objectNameInput = document.getElementById("o-name");
+  objectNameInput.addEventListener("focus", (event) => {
+    event.target.style.background = "#51b851"; // light green
+    console.log("Solidify API price fetch initiated");
+
+    const URL =
+      "https://api.solidify.ortanatech.com/action?action=upload-model&uuid=";
+    fetch(URL + getCookie("uuid"))
+      .then((response) => response.json())
+      .then(function (json) {
+        document.getElementById("payBtn").style.backgroundColor = "lightgreen";
+        jsonPriceInfoData = json; // save json data to global variable
+        //console.log(jsonPriceInfoData);
+      });
+  });
+
+  const params = new URLSearchParams(location.search);
+  var dataFromUrlParam = params.get("data");
+
+  // will load stl with query param "data" value or session storage data
+  if (dataFromUrlParam != null) {
+    loadStl(dataFromUrlParam);
+  } else {
+    loadStl(sessionStorage.getItem("data"));
+  }
 };
+
+// test data
+//STLViewer("https://3ddata.nikhilrado.repl.co/ex-scripts/will-ronan.stl", "model")
+//load_stl([2,3,1,3,4,2,5,2,3,.5,2])
